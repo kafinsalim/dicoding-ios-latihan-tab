a70de7f922932b4495ec65e501faafaecfaa6028
Kafin Salim <kafinsalim@gmail.com>
2020-04-26 14:16:22 +0700

    Initial Commit

---
+++ TryVIew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryVIew
+//
+//  Created by Admin on 26/04/20.
+//  Copyright © 2020 Kafin Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
